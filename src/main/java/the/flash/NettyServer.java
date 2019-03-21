04a1526a61656cbb69292a4542064ee38e836c60
naqi <2080252788@qq.com>
2019-03-21 17:18:43 +0800

    add annotation

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -13,25 +13,35 @@ public class NettyServer {
     private static final int BEGIN_PORT = 8000;
 
     public static void main(String[] args) {
+
+        // 线程组
         NioEventLoopGroup boosGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
+        // ServerBootstrap引导类 ,这个类将引导我们进行服务端的启动工作，直接new出来开搞。
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
+                // 线程模型
                 .group(boosGroup, workerGroup)
+                // io模型
                 .channel(NioServerSocketChannel.class)
+                // attr()方法可以给服务端的 channel，也就是NioServerSocketChannel指定一些自定义属性，
+                // 然后我们可以通过channel.attr()取出这个属性
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // 可以给每一条连接指定自定义属性，然后后续我们可以通过channel.attr()取出该属性。
                 .childAttr(clientKey, "clientValue")
+                // 可以给服务端channel设置一些属性，最常见的就是so_backlog
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                // 给每条连接设置一些TCP底层相关的属性
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                // 连接读写处理逻辑，给引导类创建一个ChannelInitializer，主要就是定义后续每条连接的数据读写，业务处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
                     }
                 });
-
 
         bind(serverBootstrap, BEGIN_PORT);
     }
