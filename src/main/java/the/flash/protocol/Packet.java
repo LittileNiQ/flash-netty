f41e34e17896365e174f0214c677cfc61f74bc9d
naqi <2080252788@qq.com>
2019-03-25 23:17:17 +0800

    add annotation

--- src/main/java/the/flash/protocol/Packet.java
+++ src/main/java/the/flash/protocol/Packet.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 public abstract class Packet {
     /**
+     * 协议中使用 @JSONField(deserialize = false,
+     * serialize = false) 标注 version 不被序列化了哦，所以协议  对象 Packet 不会出现 version
      * 协议版本
      */
     @JSONField(deserialize = false, serialize = false)
--- src/main/java/the/flash/server/ServerHandler.java
+++ src/main/java/the/flash/server/ServerHandler.java
@@ -22,18 +22,23 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
         System.out.println(new Date() + ": 客户端开始登录……");
         ByteBuf requestByteBuf = (ByteBuf) msg;
 
+        // 解码
         Packet packet = PacketCodeC.INSTANCE.decode(requestByteBuf);
 
+        // 判断是否是登录请求数据包
         if (packet instanceof LoginRequestPacket) {
             // 登录流程
             LoginRequestPacket loginRequestPacket = (LoginRequestPacket) packet;
 
             LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
             loginResponsePacket.setVersion(packet.getVersion());
+            // 登录校验
             if (valid(loginRequestPacket)) {
+                // 校验成功
                 loginResponsePacket.setSuccess(true);
                 System.out.println(new Date() + ": 登录成功!");
             } else {
+                // 校验失败
                 loginResponsePacket.setReason("账号密码校验失败");
                 loginResponsePacket.setSuccess(false);
                 System.out.println(new Date() + ": 登录失败!");
